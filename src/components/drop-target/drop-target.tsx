89a8a64a69387a60fc390bfb6c1fb47a5f7019c5
Marius P <marius.paduraru@gmail.com>
2024-04-18 22:02:24 +0100

    Add drop target button to the drop target component

--- src/components/drop-target/drop-target.tsx
+++ src/components/drop-target/drop-target.tsx
@@ -1,7 +1,15 @@
+import { IconAdd } from "../../icons/icon-add";
+
 import "./drop-target.css";
 
 const DropTarget = () => {
-  return <div className="drop-target">Drop target</div>;
+  return (
+    <div className="drop-target">
+      <button type="button">
+        <IconAdd />
+      </button>
+    </div>
+  );
 };
 
 export default DropTarget;
